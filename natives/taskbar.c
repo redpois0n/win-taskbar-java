35e74d77f50c086e493a3d3d9c79efb2b70ba438
redpois0n <red@cmail.nu>
2016-05-27 10:50:29 +0200

    First commit

---
+++ natives/taskbar.c
@@ -0,0 +1,5 @@
+#include "taskbar_Taskbar.h"
+
+JNIEXPORT void JNICALL Java_taskbar_Taskbar_setProgressState(JNIEnv *env, jclass clazz, jint hwnd, jint tbpFlags) {
+
+}
---
+++ src/taskbar/Taskbar.java
@@ -0,0 +1,7 @@
+package taskbar;
+
+public class Taskbar {
+
+	public static native void setProgressState(int hwnd, int tbpFlags);
+
+}
