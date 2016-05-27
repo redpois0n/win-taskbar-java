a06947a887a40a1b24f6b3c7dd2d13f4b211f9e1
redpois0n <red@cmail.nu>
2016-05-27 11:03:06 +0200

    SetProgressState implemented

--- natives/taskbar.c
+++ natives/taskbar.c
@@ -1,5 +1,7 @@
 #include "taskbar_Taskbar.h"
 
-JNIEXPORT void JNICALL Java_taskbar_Taskbar_setProgressState(JNIEnv *env, jclass clazz, jint hwnd, jint tbpFlags) {
+#include <windows.h>
 
+JNIEXPORT void JNICALL Java_taskbar_Taskbar_setProgressState(JNIEnv *env, jclass clazz, jint hwnd, jint tbpFlags) {
+    SetProgressState((int) hwnd, (int) tbpFlags);
 }
