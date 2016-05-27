c8b319308c0263a2e58324c184111647aebb9b45
redpois0n <red@cmail.nu>
2016-05-27 11:03:18 +0200

    SetProgressState constants added

--- src/taskbar/Taskbar.java
+++ src/taskbar/Taskbar.java
@@ -2,6 +2,12 @@ package taskbar;
 
 public class Taskbar {
 
+	public static final int TBPF_NOPROGRESS = 0x00000000;
+	public static final int TBPF_INDETERMINATE = 0x00000001;
+	public static final int TBPF_NORMAL = 0x00000002;
+	public static final int TBPF_ERROR = 0x00000004;
+	public static final int TBPF_PAUSED = 0x00000008;
+
 	public static native void setProgressState(int hwnd, int tbpFlags);
 
 }
